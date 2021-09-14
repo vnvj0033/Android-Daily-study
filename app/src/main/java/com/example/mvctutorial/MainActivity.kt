ba207b2410cf14df02761ed7836763fe0c5f09ac
sangyeob yoo <vnvj0033@gmail.com>
2021-09-14 21:26:18 +0900

    Retrofit2 가이드

--- app/src/main/java/com/example/mvctutorial/MainActivity.kt
+++ app/src/main/java/com/example/mvctutorial/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.mvctutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.mvctutorial.network.Post
+import com.example.mvctutorial.network.RetrofitServer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        loadPostWithExecute("1")
+
+        loadPostWithEnqueue("2")
+    }
+
+    private fun loadPostWithEnqueue(id: String) {
+        val callback = object: Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                Log.d(localClassName,"post = ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+            }
+
+        }
+
+        RetrofitServer.postAPI.getPost(id).enqueue(callback)
+    }
+
+    private fun loadPostWithExecute(id: String) {
+        Thread{
+            val response = RetrofitServer.postAPI.getPost(id).execute()
+
+            Log.d(localClassName,"post = ${response.body()}")
+        }.start()
     }
 }
---
+++ app/src/main/java/com/example/mvctutorial/network/RetrofitServer.kt
@@ -0,0 +1,30 @@
+package com.example.mvctutorial.network
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+object RetrofitServer {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val postAPI = retrofit.create(PostService::class.java)
+}
+
+interface PostService {
+
+    @GET("posts/{id}")
+    fun getPost(@Path("id") id: String): Call<Post>
+
+}
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
