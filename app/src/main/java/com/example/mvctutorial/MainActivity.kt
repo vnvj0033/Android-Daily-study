ddf03e5697fdbf2512d0e1611bd39bcd38efd581
vnvj0033 <vnvj0033@gmail.com>
2021-09-19 22:19:19 +0900

    레이아웃 물리적 분리 연결 시도 (코드상 Constrain을 사용해서)

--- app/src/main/java/com/example/mvctutorial/MainActivity.kt
+++ app/src/main/java/com/example/mvctutorial/MainActivity.kt
@@ -3,15 +3,37 @@ package com.example.mvctutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mvctutorial.network.Post
 import com.example.mvctutorial.network.RetrofitServer
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val mainView = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
+
+        val storyBoardView = View(this).apply {
+            layoutParams = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT).apply {
+                bottomToTop = findViewById<TextView>(R.id.cuSectionShortcut).id
+            }
+        }
+
+        val layout = ConstraintLayout(this).apply {
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            addView(mainView)
+            addView(storyBoardView)
+        }
+
+        setContentView(layout)
     }
 }
+
