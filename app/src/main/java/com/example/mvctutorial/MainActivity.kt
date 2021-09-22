5e51ccf50bbdb556603dc98e012b398dc8dd2715
sangyeob yoo <vnvj0033@gmail.com>
2021-09-22 23:21:11 +0900

    view over fragment

--- app/src/main/java/com/example/mvctutorial/MainActivity.kt
+++ app/src/main/java/com/example/mvctutorial/MainActivity.kt
@@ -6,8 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvctutorial.homecontent.HomeContentAdapter
+import com.example.mvctutorial.homecontent.StoryBoardFragment
 import com.example.mvctutorial.network.Post
 import com.example.mvctutorial.network.RetrofitServer
 import retrofit2.Call
@@ -18,20 +24,19 @@ import java.util.zip.Inflater
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val mainView = LayoutInflater.from(this).inflate(R.layout.activity_main, null, false)
+        findViewById<RecyclerView>(R.id.homeContentView).run {
+            adapter = HomeContentAdapter(30)
+        }
 
-//        val storyBoardView = View(this).apply {
-//            layoutParams = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT).apply {
-//                bottomToTop = mainView.findViewById<TextView>(R.id.cuSectionShortcut).id
-//            }
-//        }
-//        val layout = ConstraintLayout(this).apply {
-//            layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
-//            addView(mainView)
-//            addView(storyBoardView)
-//        }
-
-        setContentView(mainView)
+        findViewById<TextView>(R.id.cuSectionShortcut).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .setReorderingAllowed(true)
+                .add(R.id.content_frame, StoryBoardFragment())
+                .commit()
+        }
     }
+
 }
