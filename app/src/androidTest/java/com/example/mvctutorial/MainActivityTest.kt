4765ed253015f3ec2b5d89011b71b1ba7b426b10
vnvj0033 <vnvj0033@gmail.com>
2021-10-04 23:38:48 +0900

    UiTestActivity 의 테스트를 작성

---
+++ app/src/androidTest/java/com/example/mvctutorial/MainActivityTest.kt
@@ -0,0 +1,5 @@
+package com.example.mvctutorial
+
+import org.junit.Assert.*
+
+class MainActivityTest
---
+++ app/src/androidTest/java/com/example/mvctutorial/uitest/UiTestActivityTest.kt
@@ -0,0 +1,29 @@
+package com.example.mvctutorial.uitest
+
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.example.mvctutorial.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class UiTestActivityTest{
+
+    @get: Rule
+    val activityRule: ActivityTestRule<UiTestActivity> = ActivityTestRule(UiTestActivity::class.java, false, false)
+
+    @Test
+    fun noCountryExtra() {
+        activityRule.launchActivity(Intent())
+        onView(withId(R.id.languageTextView))
+            .check(matches(withText("korea")))
+    }
+}
