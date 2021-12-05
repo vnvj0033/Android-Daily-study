914823fedcfe214aac1bb6d730b51ea0137fbb94
vnvj0033 <vnvj0033@gmail.com>
2021-12-06 00:07:32 +0900

    디자인패턴 _11decorator 코틀린 Abst

--- app/src/main/java/com/example/mvctutorial/designpattern/_11decorator/abst/AbstAdding.kt
+++ app/src/main/java/com/example/mvctutorial/designpattern/_11decorator/abst/AbstAdding.kt
@@ -1,4 +1,6 @@
 package com.example.mvctutorial.designpattern._11decorator.abst
 
-class AbstAdding {
+class AbstAdding(private val base: IBeverage): IBeverage {
+
+    override fun getTotalPrice(): Int = base.getTotalPrice()
 }
