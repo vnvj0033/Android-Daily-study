b04cdbf887d66eae89be211b30ef23295bd95469
vnvj0033 <vnvj0033@gmail.com>
2021-12-11 15:21:32 +0900

    디자인패턴 _14facade_pattern 코틀린 컨버트 수정 자바 제거

--- app/src/main/java/com/example/mvctutorial/designpattern/_14facade_pattern/system/Facade.kt
+++ app/src/main/java/com/example/mvctutorial/designpattern/_14facade_pattern/system/Facade.kt
@@ -1,9 +1,9 @@
 package com.example.mvctutorial.designpattern._14facade_pattern.system
 
 class Facade {
-    val system01 = HelpSystem01()
-    val system02 = HelpSystem02()
-    val system03 = HelpSystem03()
+    private val system01 = HelpSystem01()
+    private val system02 = HelpSystem02()
+    private val system03 = HelpSystem03()
 
     fun process() {
         system01.process()
