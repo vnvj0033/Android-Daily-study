60a9e249dbf76447d8d295b3ae525d205dde288b
vnvj0033 <vnvj0033@gmail.com>
2021-12-12 16:52:18 +0900

    디자인패턴 state pattern 수정

--- app/src/main/java/com/example/mvctutorial/designpattern/_17state/Light.kt
+++ app/src/main/java/com/example/mvctutorial/designpattern/_17state/Light.kt
@@ -2,11 +2,8 @@ package com.example.mvctutorial.designpattern._17state
 
 open class Light {
 
-
     protected val onState : LightState = object : LightState {
-        override fun on() {
-            println("NOTING")
-        }
+        override fun on() = println("NOTING")
 
         override fun off() {
             println("Light OFF")
@@ -20,17 +17,12 @@ open class Light {
             lightState = onState
         }
 
-        override fun off() {
-            println("NOTING")
-        }
-
+        override fun off() = println("NOTING")
     }
 
     private var lightState = offState
 
     fun on() = lightState.on()
-
-
     fun off() = lightState.off()
 
 }
