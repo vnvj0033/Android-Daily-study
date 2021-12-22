fb296afb904e3e20c0845c6a28133e4e8881e4a6
vnvj0033 <vnvj0033@gmail.com>
2021-12-22 23:11:40 +0900

    rx test 짝수 구별하기

--- app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
@@ -1,9 +1,19 @@
 package com.example.mvctutorial.rxkotlin
 
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.Subject
+
 fun main() {
-    val list = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f) // 1
-    val iterator = list.iterator() // 2
-    while (iterator.hasNext()) { // 3
-        println(iterator.next()) // 4 각 엘리먼트를 출력한다
-    }
+
+    val subject: Subject<Int> = PublishSubject.create()
+
+    subject.map { isEven(it) }
+        .subscribe {
+            println("The number is ${(if (it) "Even" else "Odd")}" )
+        }
+
+    subject.onNext(4)
+    subject.onNext(9)
 }
+
+fun isEven(n: Int) : Boolean = (n % 2 == 0)
