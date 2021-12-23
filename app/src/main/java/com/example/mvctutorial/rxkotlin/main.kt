12a7a1d2a500168137beeef77062400b5d581413
vnvj0033 <vnvj0033@gmail.com>
2021-12-23 17:50:23 +0900

    observer basic

--- app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
@@ -1,19 +1,30 @@
 package com.example.mvctutorial.rxkotlin
 
-import io.reactivex.rxjava3.subjects.PublishSubject
-import io.reactivex.rxjava3.subjects.Subject
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 fun main() {
+    observerBase()
 
-    val subject: Subject<Int> = PublishSubject.create()
-
-    subject.map { isEven(it) }
-        .subscribe {
-            println("The number is ${(if (it) "Even" else "Odd")}" )
-        }
-
-    subject.onNext(4)
-    subject.onNext(9)
 }
 
-fun isEven(n: Int) : Boolean = (n % 2 == 0)
+
+fun observerBase() {
+
+    val observable1 = Observable.create<String> {
+        it.onNext("1")
+        it.onNext("2")
+        it.onError(Exception("throw error"))
+        it.onComplete()
+    }
+
+    val observer = object : Observer<String> {
+        override fun onComplete() = println("onComplete()")
+        override fun onNext(item: String) = println("onNext() - $item")
+        override fun onError(e: Throwable) = println("onError() - $e")
+        override fun onSubscribe(d: Disposable) = println("onSubscribe() - $d ")
+    }
+
+    observable1.subscribe(observer)
+}
