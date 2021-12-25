99f59b0d9a5757df73349da0e9e8814773e95a69
vnvj0033 <vnvj0033@gmail.com>
2021-12-25 17:42:23 +0900

    rx basic

--- app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/main.kt
@@ -5,39 +5,27 @@ import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
 
 fun main() {
-//    observerBase()
-    fromMethod()
-}
 
-fun fromMethod() {
-    val list = listOf(1, 2, 3)
-    val listObserver = Observable.fromIterable(list)
-
-    val observer = object : Observer<Int> {
-        override fun onSubscribe(d: Disposable) = println("onSubscribe - $d")
-        override fun onNext(t: Int) = println("onNext - $t")
-        override fun onError(e: Throwable) = println("inError - $e")
-        override fun onComplete() = println("onComplete - ")
-    }
-
-    listObserver.subscribe(observer)
-}
-
-fun observerBase() {
-
-    val observable1 = Observable.create<String> {
-        it.onNext("1")
-        it.onNext("2")
-        it.onError(Exception("throw error"))
+    val observable1 = Observable.create<Int> {
+        it.onNext(1)
+        it.onNext(2)
         it.onComplete()
     }
 
-    val observer = object : Observer<String> {
+    val observable2 = Observable.create<Int> {
+        it.onNext(1)
+        it.onNext(2)
+        it.onError(Exception("Wow!! exception"))
+    }
+
+    val observer = object : Observer<Int> {
         override fun onComplete() = println("onComplete()")
-        override fun onNext(item: String) = println("onNext() - $item")
-        override fun onError(e: Throwable) = println("onError() - $e")
+        override fun onNext(item: Int) = println("onNext() - $item")
+        override fun onError(e: Throwable) = println("onError() - $")
         override fun onSubscribe(d: Disposable) = println("onSubscribe() - $d ")
     }
 
     observable1.subscribe(observer)
+    observable2.subscribe(observer)
 }
+
