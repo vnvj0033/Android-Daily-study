81143fbfde5bcbd8281fc29ac30d828bc7985410
vnvj0033 <vnvj0033@gmail.com>
2021-12-31 19:28:34 +0900

    RxKotlin timer

--- app/src/main/java/com/example/mvctutorial/rxkotlin/_06_timer.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_06_timer.kt
@@ -8,16 +8,18 @@ import java.util.concurrent.TimeUnit
 fun main() {
     val observer = object : Observer<Long> {
         override fun onComplete() = println("onComplete()")
-        override fun onNext(t: Long) = println("onNext() - $t")
+        override fun onNext(item: Long) = println("onNext() - $item")
         override fun onError(e: Throwable) = println("onError() - $")
         override fun onSubscribe(d: Disposable) = println("onSubscribe() - $d ")
     }
 
-    // interval은 지정 시간마다 계속 호출
-    Observable.interval(1000L, TimeUnit.MILLISECONDS).subscribe(observer)
+    // timer은 정해진 시간에 한번 호출
+    Observable.timer(1000, TimeUnit.MILLISECONDS).subscribe(observer)
+
+    Thread() .start()
 
     // 0.3초간 main thread를 대기시킨다.
-    val th1 = Thread { Thread.sleep(3000) }
+    val th1 = Thread { Thread.sleep(3000)}
     th1.start()
     th1.join()
 
