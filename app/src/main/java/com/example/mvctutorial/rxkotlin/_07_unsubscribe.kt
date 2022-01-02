2233908d7be601331c65661871e71a9f98a93d32
vnvj0033 <vnvj0033@gmail.com>
2022-01-02 19:41:14 +0900

    RxKotlin unsubscribe 2

--- app/src/main/java/com/example/mvctutorial/rxkotlin/_07_unsubscribe.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_07_unsubscribe.kt
@@ -5,28 +5,50 @@ import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
 import java.util.concurrent.TimeUnit
 
-fun main() {
-    lateinit var disposable: Disposable
-    val observer = object : Observer<Long> {
-        override fun onComplete() = println("onComplete()")
-        override fun onNext(item: Long) = println("onNext() - $item")
-        override fun onError(e: Throwable) = println("onError() - $")
-        override fun onSubscribe(d: Disposable) {
-            println("onSubscribe() - $d ")
-            disposable = d
-        }
-    }
+//
+//fun main() {
+//    lateinit var disposable: Disposable
+//    val observer = object : Observer<Long> {
+//        override fun onComplete() = println("onComplete()")
+//        override fun onNext(item: Long) = println("onNext() - $item")
+//        override fun onError(e: Throwable) = println("onError() - $")
+//        override fun onSubscribe(d: Disposable) {
+//            println("onSubscribe() - $d ")
+//            disposable = d
+//        }
+//    }
+//
+//    val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
+//
+//    Thread {
+//        Thread.sleep(500)
+//        println("dispose")
+//        disposable.dispose()
+//    }.start()
+//
+//    observable.subscribe(observer) // 0.5초 대기후 dispose() 호출
+//    Thread { Thread.sleep(1000) }.apply {
+//        start()
+//        join()
+//    }
+//}
 
+fun main() {
     val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
+    val disposable = observable.subscribe(
+        { item ->
+            println("onNext - $item")
+        },
+        { err ->
+            println("onError - $err")
+        }, { println("onComplete()") }
+    ) // 0.5초 대기후 dispose() 호출
 
     Thread {
         Thread.sleep(500)
-        println("dispose")
         disposable.dispose()
-    }.start()
-
-    observable.subscribe(observer) // 0.5초 대기후 dispose() 호출
-    Thread { Thread.sleep(1000) }.apply {
+        Thread.sleep(100)
+    }.apply {
         start()
         join()
     }
