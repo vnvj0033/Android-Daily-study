2f9c819e38082815f3296e7f678318bed627050b
vnvj0033 <vnvj0033@gmail.com>
2022-01-03 19:51:25 +0900

    RxKotlin connectable observable

---
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_08_connectableObservable.kt
@@ -0,0 +1,23 @@
+package com.example.mvctutorial.rxkotlin
+
+import io.reactivex.rxjava3.kotlin.toObservable
+
+/** connectable observer은 connect를 호출하면 배출을 시작  */
+fun main() {
+    val connectableObservable = (1..10).toObservable().publish()
+    // 1번 구독자 등록
+    connectableObservable.subscribe { println("first subscriber: $it") }
+    println ("Add first subscriber.")
+
+    // 2번 구독자 등록
+    connectableObservable.map { "second subscriber: $it" }
+        .subscribe { println(it) }
+    println ("Add second subscriber.")
+
+    // 연결 시작
+    connectableObservable.connect()
+
+
+    // 3번 구독자 등록 , 실행 x
+    connectableObservable.subscribe { println("Subscription 3: $it") }
+}
