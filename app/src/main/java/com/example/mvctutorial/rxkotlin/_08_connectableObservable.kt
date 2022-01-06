78a89ce15f90c8fb56ff67ecdec313ef18a69945
vnvj0033 <vnvj0033@gmail.com>
2022-01-06 20:03:21 +0900

    RxKotlin connectable 설명 추가

--- app/src/main/java/com/example/mvctutorial/rxkotlin/_08_connectableObservable.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_08_connectableObservable.kt
@@ -4,7 +4,9 @@ import io.reactivex.rxjava3.kotlin.toObservable
 
 /** connectable observer은 connect를 호출하면 배출을 시작  */
 fun main() {
+    // publish()를 통하여 hot observable로 변경
     val connectableObservable = (1..10).toObservable().publish()
+
     // 1번 구독자 등록
     connectableObservable.subscribe { println("first subscriber: $it") }
     println ("Add first subscriber.")
@@ -17,7 +19,6 @@ fun main() {
     // 연결 시작
     connectableObservable.connect()
 
-
-    // 3번 구독자 등록 , 실행 x
+    // 3번 구독자 등록, observable이 종료된후 구독되어 실행 x
     connectableObservable.subscribe { println("Subscription 3: $it") }
 }
--- app/src/main/java/com/example/mvctutorial/rxkotlin/_09_connectable_interval.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_09_connectable_interval.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.TimeUnit
 
+/** connectable observer은 connect를 이후에도 구독되면 바로 실행  */
 fun main() {
     val connectableObservable = Observable.interval(100, TimeUnit.MILLISECONDS).publish()
 
@@ -20,7 +21,7 @@ fun main() {
 
     runBlocking { delay(300) }
 
-    // 3번 구독자 등록
+    // 3번 구독자 등록, observable 종료 전에 구독하여 실행됨
     connectableObservable.subscribe { println("3st subscriber: $it") }
 
     runBlocking { delay(300) }
