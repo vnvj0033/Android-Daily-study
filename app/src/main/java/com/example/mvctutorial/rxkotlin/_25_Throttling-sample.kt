4f389d21e1edf12cc1e1622c6b8d4e0a7015516f
vnvj0033 <vnvj0033@gmail.com>
2022-01-23 16:41:35 +0900

    주석 추가

--- app/src/main/java/com/example/mvctutorial/rxkotlin/_25_Throttling-sample.kt
+++ app/src/main/java/com/example/mvctutorial/rxkotlin/_25_Throttling-sample.kt
@@ -7,14 +7,29 @@ import java.util.concurrent.TimeUnit
 
 /**
  *  Throttling
- *   - 연속적인 emission을 수신측에서 다 처리할 수 없는 경우 특정 배출만 획득하고 나머지는 버리도록 하는 작업입니다.
- *   sample or throttleLast는 정해진 단위 시간내에 배출된 마지막 값을 단위시간이 끝날때 마다 주기적으로 획득합니다.
+ *   - 연속적인 emission을 수신측에서 다 처리할 수 없는 경우 특정 배출만 획득하고 나머지는 버리도록 하는 작업
+ *
+ *   sample or throttleLast는 정해진 단위 시간내에 배출된 마지막 값을 단위시간이 끝날때 마다 주기적으로 획득
  * */
+
 fun main() = runBlocking {
+
+    // 100ms 마다 데이터를 전달하는 observable
     val observable = Observable.interval(100, TimeUnit.MILLISECONDS)
     val time = System.currentTimeMillis()
+
+    // 200ms 마다 수신된 데이터의 마지막 값을 전달받는다.
     observable.sample(200, TimeUnit.MILLISECONDS).subscribe {
         println("Time: ${System.currentTimeMillis() - time} : $it")
     }
-    delay (1111)
+    delay(1111)
+
+    /**
+     * 수신 데이터 (전달 100ms, 수신 200ms, 받는 시간 1000ms)
+     * 200ms : 0
+     * 400ms : 2
+     * 600ms : 4
+     * 800ms : 6
+     * 1000ms : 8
+     * */
 }
