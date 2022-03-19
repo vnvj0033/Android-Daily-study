eb561b38d9b9052425de8a028b084cca8b6bf98c
vnvj0033 <vnvj0033@gmail.com>
2022-03-19 18:58:33 +0900

    coroutine - job cancel isActive

---
+++ app/src/main/java/com/example/mvctutorial/coroutines/09_loop_cancel.kt
@@ -0,0 +1,23 @@
+package com.example.mvctutorial.coroutines
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (isActive) { // computation loop, just wastes CPU
+            // yield() // 여기에 추가하면 정상적으로 취소된다
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+    delay (1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
