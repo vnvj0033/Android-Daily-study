efd422321b4971c5000f84f24aea7e89b19201cb
vnvj0033 <vnvj0033@gmail.com>
2022-03-22 20:33:24 +0900

    coroutine - lazily started sync

--- app/src/main/java/com/example/mvctutorial/coroutines/10_sequential.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/10_sequential.kt
@@ -5,7 +5,8 @@ import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking {
 //    sequentialIsDefault()
-    concurrentUsingAsync()
+//    concurrentUsingAsync()
+    lazilyStartedSync()
 }
 
 suspend fun sequentialIsDefault() = coroutineScope {
@@ -26,6 +27,19 @@ suspend fun concurrentUsingAsync() = coroutineScope {
         println("The answer is ${one.await() + two.await()}")
     }
     println("Completed in $time ms")
+}
+
+suspend fun lazilyStartedSync() = coroutineScope {
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        // some computation
+        one.start() // start the first one
+        two.start() // start the second one
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $time ms")
+
 }
 
 suspend fun doSomethingUsefulOne(): Int {
