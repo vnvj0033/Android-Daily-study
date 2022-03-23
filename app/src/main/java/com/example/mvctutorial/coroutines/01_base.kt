34f2055f9539f43ea72a8edd11f25bb0867f9728
vnvj0033 <vnvj0033@gmail.com>
2022-03-23 17:34:43 +0900

    coroutine - base 설명 추가

--- app/src/main/java/com/example/mvctutorial/coroutines/01_base.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/01_base.kt
@@ -2,6 +2,12 @@ package com.example.mvctutorial.coroutines
 
 import kotlinx.coroutines.*
 
+/**
+ * launch는 코루틴 빌더이다. 비동기 작동과 동시에 새 코루틴을 시작합니다.
+ * delay는 일시 중단 기능이다. 특정 시간 동안 코루틴을 일시 중단 합니다.
+ * runBlocking은 실행한 쓰레드의 종료를 막는 새 코루틴 필드이다. runBlocking을 시작한 쓰레드는 runBlocking이 종료되지 않으면 끝나지 않는다.
+ * GlobalScope는 전역으로 실행 가능한 코루틴 필드이다.
+ * */
 fun main() {
 //    base1()
     base2()
