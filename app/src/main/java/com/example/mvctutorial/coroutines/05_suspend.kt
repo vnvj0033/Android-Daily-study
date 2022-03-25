3717ee70ef5265e9e8a395381a14dff3aabd85fc
vnvj0033 <vnvj0033@gmail.com>
2022-03-25 18:45:54 +0900

    coroutine - 설명 추가

--- app/src/main/java/com/example/mvctutorial/coroutines/05_suspend.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/05_suspend.kt
@@ -4,6 +4,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+/**
+ * coroutine에서 사용하는 코드를 외부 함수로 빼려면 suspend 접근제어자 사용.
+ * */
 fun main() = runBlocking {
     launch { doWorld() }
     println("Hello,")
--- app/src/main/java/com/example/mvctutorial/coroutines/08_cancle.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/08_cancle.kt
@@ -1,9 +1,14 @@
 package com.example.mvctutorial.coroutines
 
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+/**
+ * Job을 통해서 코루틴을 cancle 할 수 있다.
+ * join을 사용하면 코루틴 필드의 작동를 끝까지 기다린다.
+ * */
 fun main() = runBlocking {
     val job = launch {
         repeat(1000) { i ->
@@ -15,7 +20,9 @@ fun main() = runBlocking {
     println("main: I'm tired of waiting!")
     job.cancel() // cancels the job
     job.join() // waits for job's completion
+//    job.cancelAndJoin() //
+    delay(1000)
     println("main: Now I can quit.")
-    delay(1300L) // delay a bit
+//    delay(1300L) // delay a bit
 }
 
