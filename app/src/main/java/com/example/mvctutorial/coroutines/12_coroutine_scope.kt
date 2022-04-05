0c96fb5ec91dc1e335eaa2f0773e1edea90dd266
vnvj0033 <vnvj0033@gmail.com>
2022-04-05 20:31:59 +0900

    coroutine - coroutine worker object

--- app/src/main/java/com/example/mvctutorial/coroutines/12_coroutine_scope.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/12_coroutine_scope.kt
@@ -1,12 +1,18 @@
 package com.example.mvctutorial.coroutines
 
 import kotlinx.coroutines.*
+import java.lang.StringBuilder
 import kotlin.coroutines.CoroutineContext
+
+fun main() {
+//    coroutineObject()
+    coroutineWorkerObject()
+}
 
 /**
  * 코루틴을 객체화 하는 예제 (안드로이드 생명주기에 맞도록 설계)
  * */
-fun main() = runBlocking {
+private fun coroutineObject() = runBlocking {
     val activity = Activity()
     activity.create() // create an activity
     activity.doSomething() // run test function
@@ -15,9 +21,10 @@ fun main() = runBlocking {
     println("Destroying activity!")
     activity.destroy() // cancels all coroutines
     delay(1000) // visually confirm that they don't work
+
 }
 
-class Activity : CoroutineScope {
+private class Activity : CoroutineScope {
     private lateinit var job: Job
 
     override val coroutineContext: CoroutineContext
@@ -36,5 +43,58 @@ class Activity : CoroutineScope {
                 println("Coroutine $i is done")
             }
         }
+    }
+}
+
+
+/**
+ * 코루틴을 워커로 사용하는 예제
+ * */
+private fun coroutineWorkerObject() = runBlocking {
+
+    val worker = DbWorker()
+    worker.onCreate()
+
+    worker.updateScreen()
+
+    delay(3000)
+
+    worker.onDestroy()
+
+    delay(1000)
+}
+
+private class DbWorker : CoroutineScope {
+    lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+
+    fun onCreate() { job = SupervisorJob() }
+    fun onDestroy() { job.cancel() }
+
+    fun updateScreen() {
+        launch(Dispatchers.Default) {
+            val listData = getDataFormRoom()
+            withContext(coroutineContext) {
+                setAdapter(listData)
+            }
+        }
+    }
+
+
+    private suspend fun getDataFormRoom(): List<String> {
+        delay(1500)
+        return mutableListOf("apple", "banana", "tomato")
+    }
+
+    private suspend fun setAdapter(data: List<String>) {
+        delay(1000)
+        val stringBuilder = StringBuilder().apply {
+            data.forEach { append("$it ") }
+        }
+        stringBuilder.deleteCharAt(stringBuilder.lastIndex)
+        println(stringBuilder.toString())
     }
 }
