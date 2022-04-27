c47b67b2b6d93f0d05e7e6760b2f02434b09f138
vnvj0033 <vnvj0033@gmail.com>
2022-04-27 16:43:22 +0900

    서버 응답 테스트

--- app/src/main/java/com/example/mvctutorial/network/RetrofitActivity.kt
+++ app/src/main/java/com/example/mvctutorial/network/RetrofitActivity.kt
@@ -1,29 +1,48 @@
 package com.example.mvctutorial.network
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mvctutorial.R
 import com.example.mvctutorial.network.module.*
-import com.example.mvctutorial.network.module.model.Post
 import com.example.mvctutorial.network.module.loader.PostLoader
+import com.example.mvctutorial.network.module.model.Post
 
 class RetrofitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit)
         loadPost(1)
-
     }
 
     private fun loadPost(id: Int) {
 
         val postLoader = PostLoader()
 
+        Log.d("testsyyoo", "요청")
         postLoader.getPost(id, object : DataListener<Post> {
             override fun onSuccess(data: Post) {
-                findViewById<TextView>(R.id.textView).text = data.title
+                Log.d("testsyyoo", "응답 성공")
+                findViewById<TextView>(R.id.textView).text = loadInt().toString()
+            }
+
+            override fun onFailed() {
+                Log.d("testsyyoo", "응답 실패")
             }
         })
+    }
+
+    private fun loadInt() : Int {
+        val sp = getSharedPreferences("myLocal", MODE_PRIVATE)
+        val i = sp.getInt("key", -1)
+
+        val editor = sp.edit()
+
+        try {
+            editor.putInt("key", i+1)
+            editor.commit()
+        } catch (e: Exception) { }
+        return i + 1
     }
 }
