e78740164c400f9a8c5b24effda01e9284abed3d
vnvj0033 <vnvj0033@gmail.com>
2022-05-04 21:32:41 +0900

    coroutine - Ticker channels

--- app/src/main/java/com/example/mvctutorial/coroutines/15_channels.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/15_channels.kt
@@ -14,7 +14,8 @@ fun main() {
 //    primeNumbersWithPipeline()
 //    fanOut()
 //    fanIn()
-    bufferedChannels()
+//    bufferedChannels()
+    tickerChannels()
 }
 
 /**
@@ -174,4 +175,35 @@ private fun bufferedChannels() = runBlocking {
 
     delay(1000)
     sender.cancel()
+}
+
+
+/**
+ * Ticker은 주어진 시간이 지나면 Unit을 Channel로 send
+ * */
+private fun tickerChannels() = runBlocking {
+    val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0) // create ticker channel
+    var nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+    println("Initial element is available immediately: $nextElement") // initial delay hasn't passed yet
+
+    nextElement = withTimeoutOrNull(50) { tickerChannel.receive() } // all subsequent elements has 100ms delay
+    println("Next element is not ready in 50 ms: $nextElement")
+
+    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
+    println("Next element is ready in 100 ms: $nextElement")
+
+    // Emulate large consumption delays
+    println("Consumer pauses for 150ms")
+    delay(150)
+
+    // Next element is available immediately
+    nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+    println("Next element is available immediately after large consumer delay: $nextElement")
+
+    // Note that the pause between `receive` calls is taken into account and next element arrives faster
+    nextElement = withTimeoutOrNull(60) { tickerChannel.receive() }
+    println("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
+
+    tickerChannel.cancel() // indicate that no more elements are needed
+
 }
