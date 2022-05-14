7db0247be5c39bfc614d092f6309437f63759c27
vnvj0033 <vnvj0033@gmail.com>
2022-05-14 15:00:40 +0900

    coroutine - Sequences

--- app/src/main/java/com/example/mvctutorial/coroutines/18_kotlinFlow.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/18_kotlinFlow.kt
@@ -3,7 +3,8 @@ package com.example.mvctutorial.coroutines
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    representingMultipleValues()
+//    representingMultipleValues()
+    sequences()
 }
 
 /**
@@ -11,4 +12,21 @@ fun main() {
  * */
 private fun representingMultipleValues() = runBlocking {
     listOf(1, 2, 3).forEach { value -> println(value) }
+}
+
+
+/**
+ * 자원이 소모되는 작업에는 시퀀스를 이용할 수 있다.
+ * Sequence는 객체를 생성하는 시간을 yield를 만나 값을 전달하고 객체가 생성까지 지연시킨다.
+ * */
+private fun sequences() = runBlocking {
+
+    fun foo(): Sequence<Int> = sequence { // sequence builder
+        for (i in 1..3) {
+            Thread.sleep(1000) // pretend we are computing it
+            yield(i) // yield next value
+        }
+    }
+
+    foo().forEach { println(it) }
 }
