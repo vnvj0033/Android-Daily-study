10e55a0db6fcf277e94d7fa726c381a77467a295
vnvj0033 <vnvj0033@gmail.com>
2022-05-14 14:38:35 +0900

    coroutine - Representing multiple values

---
+++ app/src/main/java/com/example/mvctutorial/coroutines/18_kotlinFlow.kt
@@ -0,0 +1,14 @@
+package com.example.mvctutorial.coroutines
+
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    representingMultipleValues()
+}
+
+/**
+ * 다수의 값은 코틀린 콜렌션을 통해 가능하다. 다음은 List를 forEach를 사용해 출력
+ * */
+private fun representingMultipleValues() = runBlocking {
+    listOf(1, 2, 3).forEach { value -> println(value) }
+}
