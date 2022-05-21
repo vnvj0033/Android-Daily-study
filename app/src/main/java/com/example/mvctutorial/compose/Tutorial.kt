ba6b8f542641b1df35b6c4bba30da074a2ae7402
vnvj0033 <vnvj0033@gmail.com>
2022-05-21 15:47:03 +0900

    compose - layouts

--- app/src/main/java/com/example/mvctutorial/compose/Tutorial.kt
+++ app/src/main/java/com/example/mvctutorial/compose/Tutorial.kt
@@ -1,8 +1,16 @@
 package com.example.mvctutorial.compose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mvctutorial.R
 
 @Composable
 fun MessageCard(name: String) {
@@ -12,5 +20,36 @@ fun MessageCard(name: String) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    MessageCard(
+        Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+}
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
