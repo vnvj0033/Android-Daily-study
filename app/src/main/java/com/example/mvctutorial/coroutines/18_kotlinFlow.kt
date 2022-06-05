b14a7f69bfc3826da534e26255535a5d22def9c4
yoosangyeob <syyoo@joara.com>
2022-06-05 22:28:12 +0900

    coroutine - Flattening flows, flatMapConcat

--- app/src/main/java/com/example/mvctutorial/coroutines/18_kotlinFlow.kt
+++ app/src/main/java/com/example/mvctutorial/coroutines/18_kotlinFlow.kt
@@ -23,7 +23,8 @@ fun main() {
 //    processingTheLatestValue()
 //    zip()
 //    combine()
-    combine2()
+//    combine2()
+    flatMapConcat()
 }
 
 /**
@@ -507,4 +508,31 @@ private fun combine2() = runBlocking {
 3 -> two at 863 ms from start
 3 -> three at 1265 ms from start
 3 -> four at 1669 ms from start
+ */
+
+
+/**
+ * flatMapConcat은 flow를 연결하는 연산자이다.
+ * */
+private fun flatMapConcat() = runBlocking {
+    fun requestFlow(i: Int): Flow<String> = flow {
+        emit("$i: First")
+        delay(500) // wait 500 ms
+        emit("$i: Second")
+    }
+
+    val startTime = System.currentTimeMillis() // remember the start time
+    (1..3).asFlow().onEach { delay(100) } // a number every 100 ms
+        .flatMapConcat { requestFlow(it) }
+        .collect { value -> // collect and print
+            println("$value at ${System.currentTimeMillis() - startTime} ms from start")
+        }
+}
+/*
+1: First at 129 ms from start
+1: Second at 631 ms from start
+2: First at 732 ms from start
+2: Second at 1233 ms from start
+3: First at 1335 ms from start
+3: Second at 1840 ms from start
  */
