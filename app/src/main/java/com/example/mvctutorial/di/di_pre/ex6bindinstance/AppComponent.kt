de9accec88aa7b9fbea7989be916cef9734bdd7e
yoosangyeob <syyoo@joara.com>
2022-09-03 15:20:48 +0900

    @Component.Builder, @BindsInstance test

--- app/src/main/java/com/example/mvctutorial/di/di_pre/ex6bindinstance/AppComponent.kt
+++ app/src/main/java/com/example/mvctutorial/di/di_pre/ex6bindinstance/AppComponent.kt
@@ -5,11 +5,15 @@ import dagger.Component
 
 @Component(modules = [AppModule::class])
 interface AppComponent {
+
     fun getApplication(): Application
+
     @Component.Builder
     interface Builder {
+
         @BindsInstance
         fun name(name: String): Builder
+
         fun build(): AppComponent
     }
 }
--- app/src/main/java/com/example/mvctutorial/di/di_pre/ex6bindinstance/Runner.kt
+++ app/src/main/java/com/example/mvctutorial/di/di_pre/ex6bindinstance/Runner.kt
@@ -1,6 +1,6 @@
 package com.example.mvctutorial.di.di_pre.ex6bindinstance
 
 fun main() {
-    val appComponent: AppComponent = DaggerAppComponent.builder().name("aa").build()
-    println(appComponent.getApplication().name) // aa
+    val appComponent: AppComponent = DaggerAppComponent.builder().name("q23123123").build()
+    println(appComponent.getApplication().toString()) // aa
 }
