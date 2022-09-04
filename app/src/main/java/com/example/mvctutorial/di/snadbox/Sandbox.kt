ecf3695fb3ee54403f971ad31c53f716bb3049be
yoosangyeob <syyoo@joara.com>
2022-09-04 20:48:45 +0900

    di snadbox - member-injection

--- app/src/main/java/com/example/mvctutorial/di/snadbox/Sandbox.kt
+++ app/src/main/java/com/example/mvctutorial/di/snadbox/Sandbox.kt
@@ -6,26 +6,29 @@ import dagger.Provides
 import javax.inject.Inject
 
 fun main() {
-    DaggerCoffeeComponent.create().make().brew()
+    val coffeeMaker = CoffeeMaker()
+// inject(), Member-injection Method 실행 (coffeeMaker객체의 멤버변수/필드에 의존성 주입)
+    DaggerCoffeeComponent.create().inject(coffeeMaker)
+    coffeeMaker.brew()
+}
+
+
+
+@Component(modules = [CoffeeMakerModule::class])
+interface CoffeeComponent {
+
+    // member-injection method 유형
+    fun inject(coffeeMaker: CoffeeMaker)
 }
 
 @Module
 class CoffeeMakerModule {
     @Provides
     fun provideHeater() : Heater = object : Heater {
-
         private var heating : Boolean = false
-
-        override fun on() {
-            heating = true
-        }
-
-        override fun off() {
-            heating = false
-        }
-
+        override fun on() { heating = true }
+        override fun off() { heating = false }
         override fun isHot(): Boolean = heating
-
     }
 
     @Provides
@@ -39,18 +42,9 @@ class CoffeeMakerModule {
     }
 }
 
-
-@Component(modules = [CoffeeMakerModule::class])
-interface CoffeeComponent {
-
-    // provision method 유형
-    fun make() : CoffeeMaker
-}
-
-class CoffeeMaker @Inject constructor(
-    private val heater: Heater,
-    private val pump: Pump
-) {
+class CoffeeMaker {
+    @Inject lateinit var heater: Heater
+    @Inject lateinit var pump: Pump
     fun brew() {
         heater.on()
         pump.pump()
